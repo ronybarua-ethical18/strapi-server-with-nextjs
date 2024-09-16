25e47d17658c85802c0a4cd484915ff6a96cdbb6
Rony <ronybarua-ethical18>
2024-09-16 10:53:12 +0600

    plugin file modified

--- config/plugins.js
+++ config/plugins.js
@@ -1,7 +1,15 @@
 module.exports = ({ env }) => ({
-    'users-permissions': {
-      config: {
-        jwtSecret: env('JWT_SECRET')
-      }
+  'users-permissions': {
+    config: {
+      jwtSecret: env('JWT_SECRET')
     }
-  });
+  },
+  upload: {
+    config: {
+      provider: 'local',
+      providerOptions: {
+        sizeLimit: 100000,
+      },
+    },
+  },
+});
