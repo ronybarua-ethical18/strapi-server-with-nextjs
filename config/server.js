c18f59f8780b42141d87dc649dfcc05d0aaa6d92
Rony <ronybarua-ethical18>
2024-09-17 19:49:46 +0600

    server file is changed

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env('HOST', '0.0.0.0'),  // Use 0.0.0.0 to accept connections from outside
+  port: env.int('PORT', process.env.PORT || 1337),  // Use Render's PORT env var
   app: {
     keys: env.array('APP_KEYS'),
   },
